cd9fc38419f29dfa9d63062331d2c5fe33ef0cad
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-09-12 09:03:01 +0200

    TitleUI script for transitioning to main scene

---
+++ Assets/Scripts/TitleUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour {
+  private void Start() {
+    Debug.Log("Title UI");
+  }
+
+  public void StartMain() {
+    Debug.Log("Start Main");
+  }
+}
